938d32e0f7b376854149b85af9d15ac3ade48528
TeleMessage <support@telemessage.com>
2015-08-28 13:16:12 +0300

    1. added Tests

--- test/parsers/json/WriterTest.php
+++ test/parsers/json/WriterTest.php
@@ -5,7 +5,8 @@ use \grinfeld\phpjsonable\utils\streams\StringInputStream;
 use \grinfeld\phpjsonable\parsers\json\Writer;
 use \grinfeld\phpjsonable\parsers\json\Json;
 use \grinfeld\phpjsonable\utils\Configuration;
-
+use \grinfeld\phpjsonable\utils\strategy\LanguageStrategyFactory;
+use \grinfeld\phpjsonable\utils\Pair;
 /**
  * @author Grinfeld Mikhail
  * @since 8/26/2015.
@@ -69,6 +70,34 @@ class WriterTest extends PHPUnit_Framework_TestCase {
         Json::encode($res, $output);
         $this->assertEquals($str, $output->toString(), "Should be \"$str\"");
 
+        $str = "[\"hello\"]";
+        $fp = new StringInputStream($str);
+        $res = Json::decode($fp);
+        $output = new StringOutputStream();
+        Json::encode($res, $output);
+        $this->assertEquals($str, $output->toString(), "Should be \"$str\"");
+
+        $str = "[\"hello\",111]";
+        $fp = new StringInputStream($str);
+        $res = Json::decode($fp);
+        $output = new StringOutputStream();
+        Json::encode($res, $output);
+        $this->assertEquals($str, $output->toString(), "Should be \"$str\"");
+
+        $str = "[{\"key1\":\"hello\",\"key2\":222},111,\"bye\",false]";
+        $fp = new StringInputStream($str);
+        $res = Json::decode($fp);
+        $output = new StringOutputStream();
+        Json::encode($res, $output);
+        $this->assertEquals($str, $output->toString(), "Should be \"$str\"");
+
+        $str = "[{\"key1\" : \"hello\",\"key2\": 222}, 111, \"bye\",[1, 2, 3]]";
+        $fp = new StringInputStream($str);
+        $res = Json::decode($fp);
+        $output = new StringOutputStream();
+        Json::encode($res, $output);
+        $this->assertEquals(str_replace(" ", "", $str), $output->toString(), "Should be \"$str\"");
+
         $str = "{\"left\":100,\"right\":\"Test\",\"class\":\"grinfeld\\phpjsonable\\utils\\Pair\"}";
         $fp = new StringInputStream($str);
         $result = Json::decode($fp);
@@ -76,12 +105,26 @@ class WriterTest extends PHPUnit_Framework_TestCase {
         Json::encode($result, $output, (new Configuration())->push(Configuration::INCLUDE_CLASS_NAME_PROPERTY, "true"));
         $this->assertEquals($str, $output->toString(), "Should be same");
 
+        $str = "{\"left\":100,\"right\":\"Test\",\"class\":\"grinfeld.phpjsonable.utils.Pair\"}";
+        $expected = "{\"left\":100,\"right\":\"Test\",\"class\":\"grinfeld.phpjsonable.utils.Pair\"}";
+        $fp = new StringInputStream($str);
+        $result = Json::decode($fp);
+        $output = new StringOutputStream();
+        Json::encode($result, $output, (new Configuration())->push(Configuration::CLASS_TYPE_PROPERTY, LanguageStrategyFactory::LANG_JAVA)->push(Configuration::INCLUDE_CLASS_NAME_PROPERTY, "true"));
+        $this->assertEquals($expected, $output->toString(), "Should be $expected");
+
         $str = "{\"left\":100,\"class\":\"grinfeld\\phpjsonable\\utils\\Pair\"}";
         $expected = "{\"left\":100,\"right\":null,\"class\":\"grinfeld\\phpjsonable\\utils\\Pair\"}";
         $fp = new StringInputStream($str);
         $result = Json::decode($fp);
         $output = new StringOutputStream();
         Json::encode($result, $output, (new Configuration())->push(Configuration::INCLUDE_CLASS_NAME_PROPERTY, "true")->push(Configuration::EXCLUDE_NULL_PROPERTY, "false"));
+        $this->assertEquals($expected, $output->toString(), "Should be $expected");
+
+        $expected = "{\"left\":100,\"right\":\"Test\",\"class\":\"grinfeld\\phpjsonable\\utils\\Pair\"}";
+        $output = new StringOutputStream();
+        $pair = new Pair(100, "Test");
+        Json::encode($pair, $output, (new Configuration())->push(Configuration::INCLUDE_CLASS_NAME_PROPERTY, "true")->push(Configuration::EXCLUDE_NULL_PROPERTY, "false"));
         $this->assertEquals($expected, $output->toString(), "Should be $expected");
 
     }
