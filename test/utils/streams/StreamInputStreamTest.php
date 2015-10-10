e5f0e45b2abe326bd62e40b27f683e031c699d0a
TeleMessage <support@telemessage.com>
2015-10-10 18:54:35 +0300

    fixed test

--- test/utils/streams/StreamInputStreamTest.php
+++ test/utils/streams/StreamInputStreamTest.php
@@ -15,7 +15,10 @@ class StreamInputStreamTest extends PHPUnit_Framework_TestCase {
         rewind($fp);
         $in = new StreamInputStream($fp);
         $this->assertEquals(true, $in->isReady(), "Just initialized. Should return isReady");
-        $in->nextChar();
+        $c = $in->nextChar();
+        // in windows the last char is eof, but in linux is always end of line before eof
+        if ($c == '\r\n' || $c == '\n')
+            $in->nextChar();
         $this->assertEquals(false, $in->isReady(), "End of string. Should return NOT isReady");
     }
 
